7c26bc2f99399bce2e370d8ff2f1dd374bbb8b9c
christophwelling <christoph.welling@desy.de>
2019-12-13 15:01:59 +0100

    Some code cleanup

--- rnonline-detector/scripts/app.js
+++ rnonline-detector/scripts/app.js
@@ -4,20 +4,14 @@ angular.module('rnonline', [])
     rnonline.stationID = 32
     rnonline.stations = [];
     rnonline.editMode = false;
-    rnonline.showDetector = function() {
-      console.log(dummyDetector.stations);
-    }
     rnonline.updateDetector = function() {
       rnonline.stations = [];
-        console.log(Object.keys(dummyDetector.stations))
-        //for (var i=0; i<Object.keys(dummyDetector.stations).length; i++) {
         for (key in dummyDetector.stations) {
           var station = dummyDetector.stations[key];
           if (station.station_id == rnonline.stationID) {
             rnonline.stations.push(station);
           }
         }
-        console.log(rnonline.stations)
     }
 
     rnonline.getStations = function() {
